5f78ee5476a4b9401e258e066c60d3eb4d908b94
Haruka I <dis0401@gmail.com>
2019-11-20 15:24:01 -0500

    Update data.py
    fixed some typos

--- stats/data.py
+++ stats/data.py
@@ -7,7 +7,7 @@ game_files.sort()
 game_frames = []
 for game_file in game_files:
     # names: column names
-    game_frame = pd.read_csv(game_file, names=['type', 'multi1', 'multi2',
+    game_frame = pd.read_csv(game_file, names=['type', 'multi2',
     'multi3', 'multi4', 'multi5', 'multi6', 'event'])
     game_frames.append(game_frame)
 
@@ -15,14 +15,14 @@ games = pd.concat(game_frames)
 
 # clean values
 # dataframe.loc[row condition, [columns]] == new value
-games.loc[games['multi5'] == '??', 'multi5'] == ''
+games.loc[games['multi5'] == '??', 'multi5'] = ''
 
 # values to be filled in for all rows on the indentifiers DataFrame
 identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
 identifiers = identifiers.fillna(method='ffill')
 
 # change the column's names
-identifiers.column = ['game_id', 'year']
+identifiers.columns = ['game_id', 'year']
 
 # append the columns of the identifiers DataFrame to the game DataFrame
 games = pd.concat([games, identifiers], axis=1, sort=False)
